46190cc44872675d717d61b25c02d198cebc68f5
scb5304 <scb5304@gmail.com>
2018-01-29 21:38:44 -0500

    Unit test UrlBuilder.

--- tests/common/utility/test_urlbuilder.py
+++ tests/common/utility/test_urlbuilder.py
@@ -8,3 +8,30 @@ class TestUrlBuilder:
             .add('world') \
             .build()
         assert url == 'hello/world'
+
+    def test_use_trailing_slash(self):
+        url_builder = UrlBuilder(use_trailing_slash=True)
+        url = url_builder.add('hello') \
+            .add('world') \
+            .add('2') \
+            .build()
+        assert url == 'hello/world/2/'
+
+    def test_use_initial_slash(self):
+        url_builder = UrlBuilder(use_initial_slash=True)
+        url = url_builder.add('hello') \
+            .add('world') \
+            .add('90') \
+            .add('1') \
+            .build()
+        assert url == '/hello/world/90/1'
+
+    def test_use_initial_and_trailing_slash(self):
+        url_builder = UrlBuilder(use_initial_slash=True, use_trailing_slash=True)
+        url = url_builder.add('hello') \
+            .add('world') \
+            .add('90') \
+            .add('1') \
+            .build()
+        assert url == '/hello/world/90/1/'
+
