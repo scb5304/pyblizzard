2848ba650fcbbb55e11612414be1ae0391e30714
scb5304 <scb5304@gmail.com>
2018-01-31 17:36:04 -0500

    Update setup.py version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages
 setup(
     name='pyblizzard',
     packages=find_packages(exclude=['tests']),
-    version='v1.0-beta.1',
+    version='v1.0-beta.2',
     description='Python3 wrapper for the Battle.net Community APIs.',
     author='Steven Brown',
     author_email='scb5304@gmail.com',
