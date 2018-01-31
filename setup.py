07213000a07b229ec11db00ced31fbfbd61aef20
scb5304 <scb5304@gmail.com>
2018-01-30 21:07:40 -0500

    Add license and setup.py.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='pyblizzard',
+    packages=['pyblizzard'],
+    version='1.0',  # Ideally should be same as your GitHub release tag varsion
+    description='Python3 wrapper for the Battle.net Community APIs.',
+    author='Steven Brown',
+    author_email='scb5304@gmail.com',
+    url='https://github.com/scb5304/pyblizzard',
+    download_url='https://github.com/scb5304/pyblizzard/archive/v1.0-beta.tar.gz',
+    keywords=['python', 'python3', 'blizzard', 'api', 'wrapper', 'battle', 'battle.net'],
+    classifiers=[],
+)
